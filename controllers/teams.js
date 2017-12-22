a8459c3845d8b00fbacb2dec5a2bff1b98f1bbba
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2017-12-21 20:23:05 -0800

    Got delete function working, tryng to figure out put without much luck yet

--- controllers/teams.js
+++ controllers/teams.js
@@ -8,20 +8,29 @@ router.get('/', function(req, res) {
   res.render('teams/index', { teams: teams });
 });
 
+router.get('/new', function(req, res) {
+  res.render('teams/new');
+});
+
 router.post('/', function(req, res) {
   teamService.addTeam(req.body);
-
   res.redirect('/teams');
 });
 
-router.get('/new', function(req, res) {
-  res.render('teams/new');
+router.delete('/:name', function(req, res){
+	teamService.deleteTeam(req.params.name);
+	res.send('We done did a delete');
+});
+
+router.put('/:name', function(req, res){
+	teamService.editTeam(req.params.name);
+	console.log('Team edited');
+	res.send('We are editing the team');
 });
 
 router.get('/:name', function(req, res) {
   // search for the team name in all the teams.
   var team = teamService.getTeam(req.params.name);
-
   res.render('teams/show', { team: team });
 });
 
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
 
 });
+
+
+// To show the form to edit
+$('.edit-link').click(function(event){
+	event.preventDefault();
+	var sectionToShow = 'edit-' + $(this).attr('href');
+	var sectionId = document.getElementById(sectionToShow);
+	$(sectionId).toggle();
+})
+
+// When the edit form is submitted
+$('#put-form').on('submit', function(event) {
+	event.preventDefault();
+	var teamElement = $(this);
+	var teamUrl = teamElement.attr('action');
+	var teamData = teamElement.serialize();
+	console.log(teamData);
+
+
+	// $.ajax({
+	// 	method: 'PUT',
+	// 	url: teamUrl,
+	// 	data: teamData
+	// }).done(function(data) {
+ //    // get data returned from the PUT route
+ //    console.log(data);
+
+ //    // do stuff when the PUT action is complete
+ //    // teamElement.remove();
+
+ //    // or, you can redirect to another page
+ //    window.location = '/teams';
+ //  });
+});
+
+
+// To delete a team
+$('.delete-link').click(function(event){
+	event.preventDefault(); //Defaut behavior of the link is to go to the URL linked
+	//But we want to intercept it and change it to delete
+	$.ajax({
+		url: $(this).attr('href'),
+		method: 'DELETE'
+	}).success(function(data){
+		window.location.href = '/'; //Reloads the current page on success
+	});
+});
